e2720841a1512a33cfa06feecf7930feb5691f1f
Francisco Machado Becker <franciscombecker@gmail.com>
2025-01-17 14:20:55 -0300

    feat: alteração para enviar mensagem aos clientes conectados ao websocket

--- app/consumer.py
+++ app/consumer.py
@@ -1,5 +1,10 @@
+from time import sleep
+
+from websocket_connection_manager import ConnectionManager
 from config import loop, KAFKA_TOPIC, KAFKA_BOOTSTRAT_SERVERS, KAFKA_CONSUMER_GROUP
 from aiokafka import AIOKafkaConsumer
+
+consumer_manager = ConnectionManager()
 
 async def consumer():
     consumer = AIOKafkaConsumer(KAFKA_TOPIC, loop=loop, bootstrap_servers=KAFKA_BOOTSTRAT_SERVERS,
@@ -8,5 +13,7 @@ async def consumer():
     try:
         async for msg in consumer:
             print(f'Consumindo mensagem: {msg}')
+            await consumer_manager.broadcast(f"Client said: {msg}")
     finally:
         await consumer.stop()
+
