99e85c5d363ef01e4e16f0c3225adf2378f8b2ec
Francisco Machado Becker <franciscombecker@gmail.com>
2025-01-17 14:21:19 -0300

    feat: criado router para websockets

---
+++ app/websocket_router.py
@@ -0,0 +1,54 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from websocket_connection_manager import ConnectionManager
+from consumer import consumer_manager
+import asyncio
+
+websocket_router = APIRouter()
+
+manager = ConnectionManager()
+
+
+@websocket_router.websocket("/websocket")
+async def ws_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    print("WebSocket connection established")
+    try:
+        while True:
+            # Aguarda mensagem do cliente
+            message = await websocket.receive_text()
+            print(f"Message received: {message}")
+
+            # Envia uma resposta de volta ao cliente
+            await websocket.send_text(f"Server received: {message}")
+    except Exception as e:
+        print(f"Connection closed: {e}")
+
+
+@websocket_router.websocket("/ws-all")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            # Recebe mensagem do cliente
+            data = await websocket.receive_text()
+            # Envia a mensagem para todos os clientes conectados
+            await manager.broadcast(f"Client said: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        print("Client disconnected")
+
+
+@websocket_router.websocket("/ws-consumer")
+async def websocket_endpoint(websocket: WebSocket):
+    await consumer_manager.connect(websocket)
+    try:
+        while True:
+            print("client connected")
+            await websocket.send_text(f"Client connect on consumer")
+            # Mantém a conexão aberta
+            # await websocket.receive_text()
+            # Mantém a conexão aberta mas não espera mensagens do front
+            await asyncio.sleep(10)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        print("Client disconnected")
