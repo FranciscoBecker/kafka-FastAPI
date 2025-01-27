ec6b67fc207dcd45c5e491c836a481e5f2b9bf35
Francisco Machado Becker <franciscombecker@gmail.com>
2025-01-27 11:42:10 -0300

    feat: criadas novas classes Channel e Transmission

--- app/schema.py
+++ app/schema.py
@@ -3,3 +3,10 @@ from pydantic import BaseModel
 
 class Message(BaseModel):
     message: str
+
+class Channel(BaseModel):
+    id: int
+
+class Transmission(BaseModel):
+    id_channel: int
+    message: str
